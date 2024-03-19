523129f19a3d87c272bd614fff80c6f646ce4ebd
Jordi <im.jordiparadelo@gmail.com>
2024-03-19 14:06:46 +0100

    ProjectDetailsList updated

--- frontend-react/components/ProjectModal/index.jsx
+++ frontend-react/components/ProjectModal/index.jsx
@@ -1,15 +1,17 @@
 "use client";
 
+import { useMemo } from "react";
 // Next
 import { useSearchParams } from "next/navigation";
 import Image from "next/image";
 // Components
-import { Button, CloseModalButton, AnimatedParagraph } from "@/components";
+import { Button, CloseModalButton, ProjectDetailsList } from "@/components";
 // Constants
 import { PROJECTS_ITEMS as projects } from "@/constants";
+// Utils
+import { splitArray } from "@/utils";
 // Styles
 import "./styles.scss";
-import {  useMemo } from "react";
 
 const ProjectModal = () => {
 	const searchParams = useSearchParams();
@@ -27,14 +29,13 @@ const ProjectModal = () => {
 	return (
 		project && (
 			<article className='project-detail'>
-				{/* <header className='project-detail__header'> */}
 				<div className='container'>
 					<div className='project__header'>
 						<div className='project__heading-wrapper'>
 							<h3 className='project__title'>{project.title}</h3>
 						</div>
 						<div className='action_wrapper'>
-							<Button href={project?.preview}>Live view</Button>
+							<Button href={project?.preview} target='_blank'>Live view</Button>
 							<CloseModalButton>Close</CloseModalButton>
 						</div>
 					</div>
@@ -89,145 +90,112 @@ const ProjectModal = () => {
 
 export default ProjectModal;
 
-export const ProjectDetailsList = ({ details }) => {
-	const { client, type, industries, year, roles, collaboration } = details;
-
-	return (
-		<div className='project-detail__details-wrapper'>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>Year</h4>
-				<p>{year}</p>
-			</div>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>Client</h4>
-				<p>{client}</p>
-			</div>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>Project Type</h4>
-				<ul className='project__categories'>
-					{type?.map((type) => (
-						<li
-							className='project__category'
-							key={type}
-						>
-							{type}
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>Industry</h4>
-				<p>
-					{industries?.map((industry) => (
-						<span key={industry}>{industry}</span>
-					))}
-				</p>
-			</div>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>Roles</h4>
-				<ul className='project__categories'>
-					{roles?.map((role) => (
-						<li
-							className='project__category'
-							key={role}
-						>
-							{role}
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className='project-detail__details-wrapper__col'>
-				<h4 className='project-detail__details-wrapper__title'>
-					Collaboration
-				</h4>
-				<ul className='project__categories'>
-					{collaboration?.map((colaborator) => (
-						<li
-							className='project__category'
-							key={colaborator}
-						>
-							{colaborator}
-						</li>
-					))}
-				</ul>
-			</div>
-		</div>
-	);
-};
- // Import useMemo hook
-
-export const ProjectGallery = ({ gallery, numOfColumns = 2 }) => { // Corrected numOfColumns variable name
-
-  // Function to split the array into columns
-  const splitArray = (array, numberOfSplits) => {
-    const chunkSize = Math.ceil(array.length / numberOfSplits);
-    const splitArray = [];
-
-    for (let i = 0; i < array.length; i += chunkSize) {
-      const chunk = array.slice(i, i + chunkSize);
-      splitArray.push(chunk);
-    }
-
-    return splitArray;
-  };
-
-  // Memoize the splitArray function result to avoid unnecessary recalculations
-  const galleryByColumns = useMemo(() => splitArray(gallery, numOfColumns), [gallery, numOfColumns]);
-
-  return (
-    <div className='project-detail__gallery' style={{'--columns': numOfColumns}}>
-      {galleryByColumns?.map((galleryColumn, columnIndex) => ( // Added columnIndex to map function
-        <ul key={columnIndex} className='project-detail__gallery-column'> {/* Changed li to ul for better semantics */}
-          {galleryColumn.map((galleryItem, index) => (
-            <li key={index} className='project-detail__gallery-item'>
-              <Image
-                src={galleryItem?.src}
-                alt={galleryItem?.alt}
-                width={galleryItem?.width}
-                height={galleryItem?.height}
-              />
-            </li>
-          ))}
-        </ul>
-      ))}
-    </div>
-  );
-};
-
-
-// export const ProjectGallery = ({ gallery }) => {
-
-// 	function splitArray(array, numberOfSplits) {
-// 		const chunkSize = Math.ceil(array.length / numberOfSplits);
-// 		const splitArray = [];
-
-// 		for (let i = 0; i < array.length; i += chunkSize) {
-// 			const chunk = array.slice(i, i + chunkSize);
-// 			splitArray.push(chunk);
-// 		}
-
-// 		return splitArray;
-// 	}
-
-// 	const galleryByColumns = useMemo(() => splitArray(gallery, numOfcolumns));
+// export const ProjectDetailsList = ({ details }) => {
+// 	const { client, type, industries, year, roles, collaboration } = details;
 
 // 	return (
-// 		<div className='project-detail__gallery'>
-// 			{galleryByColumns?.map((galleryColumn) =>
-// 				galleryColumn.map((galleryItem, index) => (
-// 					<li
-// 						key={index}
-// 						className='project-detail__gallery-item'
-// 					>
-// 						<Image
-// 							src={galleryItem?.src}
-// 							alt={galleryItem?.alt}
-// 							width={galleryItem?.width}
-// 							height={galleryItem?.height}
-// 						/>
-// 					</li>
-// 				))
-// 			)}
+// 		<div className='project-detail__details-wrapper'>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>Year</h4>
+// 				<p>{year}</p>
+// 			</div>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>Client</h4>
+// 				<p>{client}</p>
+// 			</div>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>Project Type</h4>
+// 				<ul className='project__categories'>
+// 					{type?.map((type) => (
+// 						<li
+// 							className='project__category'
+// 							key={type}
+// 						>
+// 							{type}
+// 						</li>
+// 					))}
+// 				</ul>
+// 			</div>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>Industry</h4>
+// 				<p>
+// 					{industries?.map((industry) => (
+// 						<span key={industry}>{industry}</span>
+// 					))}
+// 				</p>
+// 			</div>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>Roles</h4>
+// 				<ul className='project__categories'>
+// 					{roles?.map((role) => (
+// 						<li
+// 							className='project__category'
+// 							key={role}
+// 						>
+// 							{role}
+// 						</li>
+// 					))}
+// 				</ul>
+// 			</div>
+// 			<div className='project-detail__details-wrapper__col'>
+// 				<h4 className='project-detail__details-wrapper__title'>
+// 					Collaboration
+// 				</h4>
+// 				<ul className='project__categories'>
+// 					{collaboration?.map((colaborator) => (
+// 						<li
+// 							className='project__category'
+// 							key={colaborator}
+// 						>
+// 							{colaborator}
+// 						</li>
+// 					))}
+// 				</ul>
+// 			</div>
 // 		</div>
 // 	);
 // };
+// Import useMemo hook
+
+export const ProjectGallery = ({ gallery, numOfColumns = 2 }) => {
+	// Memoize the splitArray function result to avoid unnecessary recalculations
+	const galleryByColumns = useMemo(
+		() => splitArray(gallery, numOfColumns),
+		[gallery, numOfColumns]
+	);
+
+	return (
+		<div
+			className='project-detail__gallery'
+			style={{ "--columns": numOfColumns }}
+		>
+			{galleryByColumns?.map(
+				(
+					galleryColumn,
+					columnIndex // Added columnIndex to map function
+				) => (
+					<ul
+						key={columnIndex}
+						className='project-detail__gallery-column'
+					>
+						{" "}
+						{/* Changed li to ul for better semantics */}
+						{galleryColumn.map((galleryItem, index) => (
+							<li
+								key={index}
+								className='project-detail__gallery-item'
+							>
+								<Image
+									src={galleryItem?.src}
+									alt={galleryItem?.alt}
+									width={galleryItem?.width}
+									height={galleryItem?.height}
+								/>
+							</li>
+						))}
+					</ul>
+				)
+			)}
+		</div>
+	);
+};
--- frontend-react/utils/index.js
+++ frontend-react/utils/index.js
@@ -1,12 +1,26 @@
 export function formatString(inputString) {
-    // Replace all hashes with spaces
-    let formattedString = inputString.replace(/#/g, ' ');
+	// Replace all hashes with spaces
+	let formattedString = inputString.replace(/#/g, " ");
 
-    // Replace dashes with spaces
-    formattedString = formattedString.replace(/-/g, ' ');
+	// Replace dashes with spaces
+	formattedString = formattedString.replace(/-/g, " ");
 
-    // Capitalize the first letter of each word
-    formattedString = formattedString.replace(/\b\w/g, char => char.toUpperCase());
+	// Capitalize the first letter of each word
+	formattedString = formattedString.replace(/\b\w/g, (char) =>
+		char.toUpperCase()
+	);
 
-    return formattedString;
+	return formattedString;
+}
+
+export function splitArray(array, numberOfSplits) {
+	const chunkSize = Math.ceil(array.length / numberOfSplits);
+	const splitArray = [];
+
+	for (let i = 0; i < array.length; i += chunkSize) {
+		const chunk = array.slice(i, i + chunkSize);
+		splitArray.push(chunk);
+	}
+
+	return splitArray;
 }
