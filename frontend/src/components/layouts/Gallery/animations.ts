a3bfbf670f083f4f664b7126b368e5b98fad7e9c
Jordi <im.jordiparadelo@gmail.com>
2026-04-23 12:45:34 +0200

    Remove unused gallery animations file to streamline the codebase and improve maintainability.

---
+++ frontend/src/components/layouts/Gallery/animations.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export const galleryAnimations = (
+  element: React.RefObject<HTMLDivElement | null>,
+) => {
+  const OFFSET_TRANSLATION = 20;
+  gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+  useGSAP(
+    () => {
+      if (!element) return;
+      const screenHeight = window.innerHeight;
+
+      gsap.set(element?.current, {
+        transform: `translateX(${-OFFSET_TRANSLATION}%)`,
+      });
+
+      const animation = gsap.to(element?.current, {
+        translateX: 100,
+      });
+
+      ScrollTrigger.create({
+        animation: animation,
+        trigger: element.current,
+        scrub: true,
+        start: `top+=20% bottom`,
+        end: `bottom+=${screenHeight} bottom`,
+        markers: true,
+      });
+    },
+    { scope: element },
+  );
+};
