2eb90dc3b7dcf526c845480a8aed3355e7af558d
Jordi <im.jordiparadelo@gmail.com>
2026-04-23 13:15:10 +0200

    Refactor image URL handling across components for consistency and improved maintainability
    - Updated image URL formatting functions to use formatStrapiMediaUrl for consistent media handling.
    - Refactored BusinessMarquee, GallerySlideShow, ProjectItem, and AvatarDropdown components to utilize the new image URL function.
    - Enhanced overall code readability and structure by standardizing image URL processing.

--- frontend/src/components/layouts/Gallery/animations.ts
+++ frontend/src/components/layouts/Gallery/animations.ts
@@ -6,33 +6,32 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export const galleryAnimations = (
-  element: React.RefObject<HTMLDivElement | null>,
+	element: React.RefObject<HTMLDivElement | null>,
 ) => {
-  const OFFSET_TRANSLATION = 20;
-  gsap.registerPlugin(ScrollTrigger, useGSAP);
+	const OFFSET_TRANSLATION = 20;
+	gsap.registerPlugin(ScrollTrigger, useGSAP);
 
-  useGSAP(
-    () => {
-      if (!element) return;
-      const screenHeight = window.innerHeight;
+	useGSAP(
+		() => {
+			if (!element) return;
+			const screenHeight = window.innerHeight;
 
-      gsap.set(element?.current, {
-        transform: `translateX(${-OFFSET_TRANSLATION}%)`,
-      });
+			gsap.set(element?.current, {
+				transform: `translateX(${-OFFSET_TRANSLATION}%)`,
+			});
 
-      const animation = gsap.to(element?.current, {
-        translateX: 100,
-      });
+			const animation = gsap.to(element?.current, {
+				translateX: 100,
+			});
 
-      ScrollTrigger.create({
-        animation: animation,
-        trigger: element.current,
-        scrub: true,
-        start: `top+=20% bottom`,
-        end: `bottom+=${screenHeight} bottom`,
-        markers: true,
-      });
-    },
-    { scope: element },
-  );
+			ScrollTrigger.create({
+				animation: animation,
+				trigger: element.current,
+				scrub: true,
+				start: `top+=20% bottom`,
+				end: `bottom+=${screenHeight} bottom`,
+			});
+		},
+		{ scope: element },
+	);
 };
