5d9d28b8697aa0af07b990d4e54e85bd847ea4c3
Jordi <im.jordiparadelo@gmail.com>
2026-04-26 23:56:26 +0200

    Refactor Gallery components for improved readability and functionality
    - Standardized indentation and formatting in GallerySlideShow and Gallery components for better maintainability.
    - Removed unused imports and commented-out code to streamline the codebase.
    - Enhanced styles in Gallery and AboutSection for consistent layout and improved responsiveness.
    - Deleted obsolete animations file from ProjectsSection to clean up the project structure.

--- frontend/src/components/layouts/ProjectsSection/ProjectsList.tsx
+++ frontend/src/components/layouts/ProjectsSection/ProjectsList.tsx
@@ -1,23 +1,15 @@
-"use client";
-
-import { useRef } from "react";
 import type { StrapiProject } from "@/lib/types";
-import useListAnimation from "./animations";
 import ProjectItem from "./ProjectItem";
 import styles from "./styles.module.css";
 
 const ProjectsList = ({ data }: { data: StrapiProject[] }) => {
-  const componentRef = useRef(null);
-
-  useListAnimation(componentRef);
-
-  return (
-    <ul className={styles["projects-list"]} ref={componentRef}>
-      {data.map((project) => (
-        <ProjectItem key={project.title} project={project} />
-      ))}
-    </ul>
-  );
+	return (
+		<ul className={styles["projects-list"]}>
+			{data.map((project) => (
+				<ProjectItem key={project.title} project={project} />
+			))}
+		</ul>
+	);
 };
 
 export default ProjectsList;
