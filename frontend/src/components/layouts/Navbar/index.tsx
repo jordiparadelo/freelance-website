4062507e7fbe7b5abe5bf6294d1fcdda7b7b5959
Jordi <im.jordiparadelo@gmail.com>
2026-04-29 01:31:35 +0200

    Refactor Navbar and AvatarDropdown components for improved readability and consistency
    - Standardized indentation and formatting in Navbar and AvatarDropdown components for better code clarity.
    - Ensured consistent use of async/await syntax in Navbar for fetching business information.
    - Enhanced the structure of the AvatarDropdown component for improved maintainability and user interaction.

--- frontend/src/components/layouts/Navbar/index.tsx
+++ frontend/src/components/layouts/Navbar/index.tsx
@@ -1,26 +1,27 @@
 import { ROUTES } from "@/app/site.config";
 import { AvatarDropdown } from "@/components/ui";
+import { getBusinessInfo } from "@/lib/db";
+
 import NavActions from "./NavActions";
 import NavMenu from "./NavMenu";
 import "./styles.scss";
-import { getBusinessInfo } from "@/lib/db";
 
 const Navbar = async () => {
-	const BUSINESS_INFO = await getBusinessInfo();
+  const BUSINESS_INFO = await getBusinessInfo();
 
-	return (
-		<nav className="navbar">
-			<div className="padding-global">
-				<div className="container">
-					<div className="navbar__layout">
-						<AvatarDropdown data={BUSINESS_INFO} />
-						<NavMenu links={ROUTES} />
-						<NavActions data={BUSINESS_INFO} />
-					</div>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <div className="padding-global">
+        <div className="container">
+          <div className="navbar__layout">
+            <AvatarDropdown data={BUSINESS_INFO} />
+            <NavMenu links={ROUTES} />
+            <NavActions data={BUSINESS_INFO} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- frontend/src/components/ui/AvatarDropdown/index.tsx
+++ frontend/src/components/ui/AvatarDropdown/index.tsx
@@ -11,160 +11,160 @@ import useDropdownAnimation from "./animations";
 import styles from "./styles.module.scss";
 
 const AvatarDropdown = ({ data }: { data: BusinessInfo }) => {
-	// State to manage dropdown open/close status
+  // State to manage dropdown open/close status
 
-	const [isDropdownOpen, setDropdownOpen] = useState(false);
-	const dropdownRef = useRef<HTMLDivElement | null>(
-		null,
-	) as RefObject<HTMLDivElement>;
-	const { openMenu, closeMenu } = useDropdownAnimation(dropdownRef);
-	const { legalName, country, city, social_links: links, cv, avatar } = data;
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(
+    null,
+  ) as RefObject<HTMLDivElement>;
+  const { openMenu, closeMenu } = useDropdownAnimation(dropdownRef);
+  const { legalName, country, city, social_links: links, cv, avatar } = data;
 
-	const AVATAR_SRC = formatStrapiMediaUrl(avatar.url);
+  const AVATAR_SRC = formatStrapiMediaUrl(avatar.url);
 
-	const DOWNLOADABLE_CV = formatStrapiMediaUrl(cv?.url);
+  const DOWNLOADABLE_CV = formatStrapiMediaUrl(cv?.url);
 
-	// Function to handle clicks outside the dropdown
-	const handleClicksOutsideDropdown = useCallback(
-		(event: MouseEvent | TouchEvent | FocusEvent): void => {
-			if (!dropdownRef.current) return;
-			const target = event.target;
-			if (!(target instanceof Node) || dropdownRef.current.contains(target)) {
-				return;
-			}
-			setDropdownOpen(false);
-			closeMenu();
-		},
-		[closeMenu],
-	);
+  // Function to handle clicks outside the dropdown
+  const handleClicksOutsideDropdown = useCallback(
+    (event: MouseEvent | TouchEvent | FocusEvent): void => {
+      if (!dropdownRef.current) return;
+      const target = event.target;
+      if (!(target instanceof Node) || dropdownRef.current.contains(target)) {
+        return;
+      }
+      setDropdownOpen(false);
+      closeMenu();
+    },
+    [closeMenu],
+  );
 
-	// Function to handle Escape key press
-	const handleEscDropdown = useCallback(
-		(event: KeyboardEvent): void => {
-			if (event.key === "Escape") {
-				setDropdownOpen(false);
-				closeMenu();
-			}
-		},
-		[closeMenu],
-	);
+  // Function to handle Escape key press
+  const handleEscDropdown = useCallback(
+    (event: KeyboardEvent): void => {
+      if (event.key === "Escape") {
+        setDropdownOpen(false);
+        closeMenu();
+      }
+    },
+    [closeMenu],
+  );
 
-	useOnClickOutside(dropdownRef, handleClicksOutsideDropdown);
-	useEventListener("keydown", handleEscDropdown);
+  useOnClickOutside(dropdownRef, handleClicksOutsideDropdown);
+  useEventListener("keydown", handleEscDropdown);
 
-	// Function to toggle the dropdown open state
-	const toggleDropdownOpen = () => {
-		setDropdownOpen((prev) => !prev);
-		isDropdownOpen ? closeMenu() : openMenu();
-	};
+  // Function to toggle the dropdown open state
+  const toggleDropdownOpen = () => {
+    setDropdownOpen((prev) => !prev);
+    isDropdownOpen ? closeMenu() : openMenu();
+  };
 
-	return (
-		<div
-			className={styles["avatar-dropdown"]}
-			ref={dropdownRef}
-			data-expanded={isDropdownOpen}
-		>
-			<button
-				type="button"
-				onClick={toggleDropdownOpen}
-				aria-haspopup="menu"
-				aria-expanded={isDropdownOpen}
-				className={styles["avatar-dropdown__button"]}
-				data-target="button"
-			>
-				<div className={styles["avatar-dropdown__avatar"]}>
-					<Image
-						src={AVATAR_SRC}
-						height={64}
-						width={64}
-						alt="Jordi Paradelo - Freelance Designer & Developer"
-						className={styles["avatar-dropdown__avatar-image"]}
-					/>
-				</div>
-				<ArrowDownIcon />
-			</button>
+  return (
+    <div
+      className={styles["avatar-dropdown"]}
+      ref={dropdownRef}
+      data-expanded={isDropdownOpen}
+    >
+      <button
+        type="button"
+        onClick={toggleDropdownOpen}
+        aria-haspopup="menu"
+        aria-expanded={isDropdownOpen}
+        className={styles["avatar-dropdown__button"]}
+        data-target="button"
+      >
+        <div className={styles["avatar-dropdown__avatar"]}>
+          <Image
+            src={AVATAR_SRC}
+            height={64}
+            width={64}
+            alt="Jordi Paradelo - Freelance Designer & Developer"
+            className={styles["avatar-dropdown__avatar-image"]}
+          />
+        </div>
+        <ArrowDownIcon />
+      </button>
 
-			<div
-				role="menu"
-				aria-hidden={!isDropdownOpen}
-				hidden={!isDropdownOpen}
-				className={styles["avatar-dropdown__menu"]}
-				data-target="menu"
-			>
-				{/* Contact Details */}
-				<div className={styles["avatar-dropdown__menu-block"]}>
-					<p className="heading-style-uppercase">Contact details</p>
-					<ul className={styles["avatar-dropdown__menu-list"]}>
-						{data.social_links
-							.filter((link) => link.type === "email")
-							.map((contact) => (
-								<li
-									key={contact?.nameID}
-									className={styles["avatar-dropdown__menu-list-item"]}
-								>
-									<a
-										className={styles["avatar-dropdown__menu-link"]}
-										href={contact.href}
-									>
-										{contact.href}
-									</a>
-								</li>
-							))}
-					</ul>
-				</div>
+      <div
+        role="menu"
+        aria-hidden={!isDropdownOpen}
+        hidden={!isDropdownOpen}
+        className={styles["avatar-dropdown__menu"]}
+        data-target="menu"
+      >
+        {/* Contact Details */}
+        <div className={styles["avatar-dropdown__menu-block"]}>
+          <p className="heading-style-uppercase">Contact details</p>
+          <ul className={styles["avatar-dropdown__menu-list"]}>
+            {data.social_links
+              .filter((link) => link.type === "email")
+              .map((contact) => (
+                <li
+                  key={contact?.nameID}
+                  className={styles["avatar-dropdown__menu-list-item"]}
+                >
+                  <a
+                    className={styles["avatar-dropdown__menu-link"]}
+                    href={contact.href}
+                  >
+                    {contact.href}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </div>
 
-				{/* Business Details */}
-				<div className={styles["avatar-dropdown__menu-block"]}>
-					<p className="heading-style-uppercase">Business details</p>
-					<ul className={styles["avatar-dropdown__menu-list"]}>
-						<li className={styles["avatar-dropdown__menu-list-item"]}>
-							<span>{legalName}</span>
-						</li>
-						<li className={styles["avatar-dropdown__menu-list-item"]}>
-							<span>
-								Location: {country}, {city}
-							</span>
-						</li>
-					</ul>
-				</div>
+        {/* Business Details */}
+        <div className={styles["avatar-dropdown__menu-block"]}>
+          <p className="heading-style-uppercase">Business details</p>
+          <ul className={styles["avatar-dropdown__menu-list"]}>
+            <li className={styles["avatar-dropdown__menu-list-item"]}>
+              <span>{legalName}</span>
+            </li>
+            <li className={styles["avatar-dropdown__menu-list-item"]}>
+              <span>
+                Location: {country}, {city}
+              </span>
+            </li>
+          </ul>
+        </div>
 
-				{/* Social Media Links */}
-				<div className={styles["avatar-dropdown__menu-block"]}>
-					<p className="heading-style-uppercase">Socials</p>
-					<ul className={styles["avatar-dropdown__menu-list"]}>
-						{links
-							.filter((link) => link.type === "website")
-							.map((link) => (
-								<li
-									key={link.nameID}
-									className={styles["avatar-dropdown__menu-list-item"]}
-								>
-									<a
-										className={styles["avatar-dropdown__menu-link"]}
-										href={link.href}
-										target="_blank"
-										rel="noopener noreferrer"
-									>
-										{link.displayName}
-									</a>
-								</li>
-							))}
-					</ul>
-				</div>
+        {/* Social Media Links */}
+        <div className={styles["avatar-dropdown__menu-block"]}>
+          <p className="heading-style-uppercase">Socials</p>
+          <ul className={styles["avatar-dropdown__menu-list"]}>
+            {links
+              .filter((link) => link.type === "website")
+              .map((link) => (
+                <li
+                  key={link.nameID}
+                  className={styles["avatar-dropdown__menu-list-item"]}
+                >
+                  <a
+                    className={styles["avatar-dropdown__menu-link"]}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {link.displayName}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </div>
 
-				{/* Additional Actions */}
-				<div className={styles["avatar-dropdown__menu-actions"]}>
-					<Button
-						href={DOWNLOADABLE_CV}
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Download CV
-					</Button>
-				</div>
-			</div>
-		</div>
-	);
+        {/* Additional Actions */}
+        <div className={styles["avatar-dropdown__menu-actions"]}>
+          <Button
+            href={DOWNLOADABLE_CV}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Download CV
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AvatarDropdown;
