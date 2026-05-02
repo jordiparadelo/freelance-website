6f54a93390a061cce4cccd1ec7abb31654a8c48f
Jordi <im.jordiparadelo@gmail.com>
2026-05-02 22:58:12 +0200

    Refactor ProjectsTable component and update styles for improved readability and consistency
    - Standardized indentation and formatting in ProjectsTable component for better code clarity.
    - Enhanced layout and styling in ProjectPage's CSS for improved user experience.
    - Cleaned up unused styles and ensured consistent use of CSS properties across components.

--- frontend/src/components/layouts/ProjectsTable/index.tsx
+++ frontend/src/components/layouts/ProjectsTable/index.tsx
@@ -8,83 +8,80 @@ import { projectsTableAnimations } from "./animations";
 import styles from "./styles.module.css";
 
 const ProjectsTable = ({ projects }: { projects: Project[] }) => {
-  const componentRef = useRef<HTMLDivElement | null>(null);
-  const [selected, setSelected] = useState<string | null>(null);
+	const componentRef = useRef<HTMLDivElement | null>(null);
+	const [selected, setSelected] = useState<string | null>(null);
 
-  projectsTableAnimations(componentRef);
+	projectsTableAnimations(componentRef);
 
-  return (
-    <div className={styles["projects-table"]} ref={componentRef}>
-      <div className={styles["table_header"]}>
-        <div className={styles["list_row"]}>
-          {["Title", "Industries", "Collaborator", "Year"].map((label) => (
-            <div key={label} className={styles["list_cell"]}>
-              <span>{label}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className={styles["table_body"]}>
-        <figure className={styles["projects-card"]} data-target="card">
-          {projects
-            .filter((project) => project.id === selected)
-            .map((project) => {
-              const imageSrc = formatStrapiMediaUrl(
-                project.image.url as string,
-              );
-              return (
-                <Image
-                  key={project.id}
-                  src={imageSrc}
-                  alt={project.title}
-                  width={200}
-                  height={400}
-                />
-              );
-            })}
-        </figure>
-        <ul className={styles["list"]}>
-          {projects.map((project) => (
-            <li
-              key={project.id}
-              className={styles["list_item"]}
-              data-target="listItem"
-            >
-              <Link
-                href={`/projects/${project.nameID}`}
-                className={styles["list_row"]}
-                onMouseEnter={() => setSelected(project.id)}
-              >
-                <div className={styles["list_cell"]}>
-                  <h3 className="heading-style-h4">{project.title}</h3>
-                </div>
-                {/* <div className={styles["list_cell"]}>
-													<p>{project.challenge}</p>
-												</div> */}
-                <div className={styles["list_cell"]}>
-                  <div className="details-group">
-                    {project.details?.industries.map((industry) => (
-                      <p key={industry.label}>{industry.label}</p>
-                    ))}
-                  </div>
-                </div>
-                <div className={styles["list_cell"]}>
-                  <div className="details-group">
-                    {project.details?.collaboration.map((collaborator) => (
-                      <p key={collaborator.label}>{collaborator.label}</p>
-                    ))}
-                  </div>
-                </div>
-                <div className={styles["list_cell"]}>
-                  <p>{project.details?.year}</p>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles["projects-table"]} ref={componentRef}>
+			<div className={styles["table_header"]}>
+				<div className={styles["list_row"]}>
+					{["Title", "Industries", "Collaborator", "Year"].map((label) => (
+						<div key={label} className={styles["list_cell"]}>
+							<span>{label}</span>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className={styles["table_body"]}>
+				<figure className={styles["projects-card"]} data-target="card">
+					{projects
+						.filter((project) => project.id === selected)
+						.map((project) => {
+							const imageSrc = formatStrapiMediaUrl(
+								project.image.url as string,
+							);
+							return (
+								<Image
+									key={project.id}
+									src={imageSrc}
+									alt={project.title}
+									width={200}
+									height={400}
+								/>
+							);
+						})}
+				</figure>
+				<ul className={styles["list"]}>
+					{projects.map((project) => (
+						<li
+							key={project.id}
+							className={styles["list_item"]}
+							data-target="listItem"
+						>
+							<Link
+								href={`/projects/${project.nameID}`}
+								className={styles["list_row"]}
+								onMouseEnter={() => setSelected(project.id)}
+							>
+								<div className={styles["list_cell"]}>
+									<h3 className="heading-style-h4">{project.title}</h3>
+								</div>
+								<div className={styles["list_cell"]}>
+									<div className="details-group">
+										{project.details?.industries.map((industry) => (
+											<p key={industry.label}>{industry.label}</p>
+										))}
+									</div>
+								</div>
+								<div className={styles["list_cell"]}>
+									<div className="details-group">
+										{project.details?.collaboration.map((collaborator) => (
+											<p key={collaborator.label}>{collaborator.label}</p>
+										))}
+									</div>
+								</div>
+								<div className={styles["list_cell"]}>
+									<p>{project.details?.year}</p>
+								</div>
+							</Link>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
 
 export default ProjectsTable;
