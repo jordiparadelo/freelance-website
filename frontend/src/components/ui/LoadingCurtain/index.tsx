2d80160394f971d2ca713e144efce248d1461287
Jordi <im.jordiparadelo@gmail.com>
2026-05-07 16:09:42 +0200

    Refactor LoadingCurtain and ProcessApproach components for improved functionality and layout
    - Updated LoadingCurtain to include a progress circle and avatar, enhancing the loading experience.
    - Refactored animations for better performance and visual feedback during loading.
    - Improved ProcessApproach component structure for clearer rendering of process steps and enhanced image handling.
    - Streamlined CSS styles for better organization and responsiveness across components.

--- frontend/src/components/ui/LoadingCurtain/index.tsx
+++ frontend/src/components/ui/LoadingCurtain/index.tsx
@@ -1,23 +1,67 @@
 "use client";
+import Image, { type StaticImageData } from "next/image";
 import { useRef } from "react";
+import AvatarPic from "../../../../public/images/avatar-stripe.png";
+import Curves from "../Curves";
 import { loadingAnimation } from "./animations";
 import styles from "./styles.module.css";
 
 const LoadingCurtain = () => {
-  const componentRef = useRef(null);
+	const componentRef = useRef(null);
 
-  loadingAnimation(componentRef);
+	loadingAnimation(componentRef);
 
-  return (
-    <div className={styles["loading-curtain"]} ref={componentRef}>
-      <div className={styles["layout"]}>
-        <div data-target="loading-bar" className={styles["loading-bar"]}>
-          <div className={styles["loading-bar_progress"]}></div>
-        </div>
-        <span data-target="text">Loading</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles["loading-curtain"]} ref={componentRef}>
+			<div className={styles["layout"]}>
+				<ProgressCircle />
+				<Avatar />
+			</div>
+			<span className={styles["progress-number"]} data-target="progress-number">
+				0
+			</span>
+			<Curves orientation="top" fill="var(--background-color--base)" />
+		</div>
+	);
+};
+
+const Avatar = () => {
+	return <div data-target="avatar" className={styles["avatar"]}></div>;
+};
+
+type ProgressCircleProps = {
+	stroke?: number;
+	size?: number;
+};
+
+const ProgressCircle = (props: ProgressCircleProps) => {
+	return (
+		<svg
+			width="100%"
+			height="100%"
+			viewBox="0 0 64 64"
+			data-target="progress-circle"
+		>
+			<title>Progress Bar</title>
+			<circle
+				cx="32"
+				cy="32"
+				r="31"
+				stroke="currentColor"
+				strokeWidth={props.stroke || 0.5}
+				fill="none"
+				id="progress-path"
+			/>
+			<circle
+				cx="32"
+				cy="32"
+				r="31"
+				stroke="rgba(from currentColor r g b / 10%)"
+				strokeWidth={props.stroke || 0.5}
+				fill="none"
+			/>
+		</svg>
+	);
 };
 
 export default LoadingCurtain;
--- frontend/src/lib/context/SiteProvider.tsx
+++ frontend/src/lib/context/SiteProvider.tsx
@@ -1,18 +1,18 @@
 import type React from "react";
-// import { LoadingCurtain } from "@/components/ui";
+import { LoadingCurtain } from "@/components/ui";
 import ScrollProvider from "@/lib/context/ScrollContext";
 import { ThemeProvider } from "@/lib/context/ThemeProvider";
 import { TransitionPageProvider } from "@/lib/context/TransitionPageContext";
 
 const SiteProvider = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ThemeProvider>
-      <TransitionPageProvider>
-        {/* <LoadingCurtain /> */}
-        <ScrollProvider>{children}</ScrollProvider>
-      </TransitionPageProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider>
+			<TransitionPageProvider>
+				<LoadingCurtain />
+				<ScrollProvider>{children}</ScrollProvider>
+			</TransitionPageProvider>
+		</ThemeProvider>
+	);
 };
 
 export default SiteProvider;
