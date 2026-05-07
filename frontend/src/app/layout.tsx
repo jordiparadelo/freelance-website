491c0a11ce9297eb419a049e9d2a7b435596ddd7
Jordi <im.jordiparadelo@gmail.com>
2026-05-07 16:44:05 +0200

    Refactor layout components for improved structure and readability
    - Updated RootLayout to include a <main> tag for better semantic structure.
    - Enhanced GallerySlideShow component with consistent formatting and added 'loading' attribute to images for performance optimization.
    - Improved code readability by standardizing indentation and formatting across both components.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -12,22 +12,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = METADATA;
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <JsonLd data={ORGANIZATION_DATA} />
-      </head>
-      <body className={inter.className}>
-        <SiteProvider>
-          <Navbar />
-          {children}
-          <Footer />
-        </SiteProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<JsonLd data={ORGANIZATION_DATA} />
+			</head>
+			<body className={inter.className}>
+				<SiteProvider>
+					<Navbar />
+					<main>{children}</main>
+					<Footer />
+				</SiteProvider>
+			</body>
+		</html>
+	);
 }
--- frontend/src/components/layouts/Gallery/GallerySlideShow.tsx
+++ frontend/src/components/layouts/Gallery/GallerySlideShow.tsx
@@ -6,25 +6,26 @@ import { formatStrapiMediaUrl } from "@/lib/db";
 import type { GalleryImage } from "@/lib/db/types";
 
 const GallerySlideShow = ({ images }: { images: GalleryImage[] }) => {
-  const componentRef = useRef(null);
+	const componentRef = useRef(null);
 
-  return (
-    <div className="gallery__slideshow" ref={componentRef}>
-      {images.map((image) => {
-        const imageSrc = formatStrapiMediaUrl(image.src.url);
-        return (
-          <figure className="gallery__slide" key={image.id}>
-            <Image
-              src={imageSrc}
-              alt={image.src.alternativeText || ""}
-              width={image.src.width}
-              height={image.src.height}
-            />
-          </figure>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="gallery__slideshow" ref={componentRef}>
+			{images.map((image) => {
+				const imageSrc = formatStrapiMediaUrl(image.src.url);
+				return (
+					<figure className="gallery__slide" key={image.id}>
+						<Image
+							src={imageSrc}
+							alt={image.src.alternativeText || ""}
+							width={image.src.width}
+							height={image.src.height}
+							loading="eager"
+						/>
+					</figure>
+				);
+			})}
+		</div>
+	);
 };
 
 export default GallerySlideShow;
